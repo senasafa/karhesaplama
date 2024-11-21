d4ca80572bfc990a5c0a68ae217c0b44c92e7c0a
senasafa <senasafa8@gmail.com>
2024-11-22 00:17:32 +0300

    Add files via upload

---
+++ kar hesaplama.cs
@@ -0,0 +1,25 @@
+﻿using System;
+internal class Program
+{
+    private const int V = 100;
+
+    private static void Main(string[] args)
+    {
+        int gelir = 1200;
+        Console.WriteLine(gelir);
+        int gider = 850;
+        Console.WriteLine(gider);
+        Console.WriteLine(gelir >= gider);
+        if (gelir >= gider) ;
+        {
+            Console.WriteLine(gelir - gider);
+            int V = 100;
+            Console.WriteLine((gelir - gider) * V / gider);
+            Console.WriteLine("kardasın");
+        }
+
+    }
+
+
+    }  
+    
